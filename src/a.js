6e4dba1d0901a43bdc06b6d4abe06f870bb0b86a
Queendomking <511154550@qq.com>
2020-04-10 22:24:41 +0800

    first commit

---
+++ src/a.js
@@ -0,0 +1,14 @@
+
+
+function arrowl(){
+    document.getElementById('arrowl').src="photo/arrow l-hover.png";
+}    
+function unarrowl() {
+    document.getElementById('arrowl').src="photo/arrow l.png";
+}
+function arrowr(){
+    document.getElementById('arrowr').src="photo/arrow r-hover.png";
+}    
+function unarrowr() {
+    document.getElementById('arrowr').src="photo/arrow r.png";
+}
